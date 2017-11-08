a3083465c9ea6203b4ea02ed902cb0792e351997
kfrasier <kaitfrasier@gmail.com>
2017-11-08 13:58:00 -0800

    Add KL divergence metric

---
+++ HabitatProject/R/weighted_logloss.R
@@ -0,0 +1,8 @@
+weighted_logloss <- function (y_pred, y_true, y_weights) 
+{
+  eps <- 1e-15
+  y_pred <- pmax(pmin(y_pred, 1 - eps), eps)
+  LogLoss <- -mean((y_true * log(y_pred) + (1 - y_true) * log(1 - y_pred))
+                   * y_weights,na.rm = TRUE)
+  return(LogLoss)
+}
