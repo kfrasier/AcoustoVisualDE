ecd7da078d747c232766e3692f3c3bb307eb52d3
kfrasier <kaitfrasier@gmail.com>
2019-09-27 17:43:26 -0700

    nevermind. Weighted, and RMSE implemented

--- knit_everything.R
+++ knit_everything.R
@@ -1,54 +1,50 @@
 setwd('E:/NASData/AcoustoVisualDE/AcoustoVisualDE')
 
-myList =  c("Ssp_model_runs_gam.Rmd", "Pm_model_runs_gam.Rmd", "Zc_model_runs_gam.Rmd", "Gg_model_runs_gam.Rmd")
-# myList = c("Ssp_model_runs_gam_density.Rmd","Gg_model_runs_gam_density.Rmd","Pm_model_runs_gam_density.Rmd","Zc_model_runs_gam_density.Rmd")
-   
-#  myList = c("Ssp_model_runs_NN.Rmd","Ssp_model_runs_NN_density.Rmd","Gg_model_runs_NN.Rmd","Gg_model_runs_NN_density.Rmd",
- #         "Pm_model_runs_NN.Rmd","Pm_model_runs_NN_density.Rmd","Zc_model_runs_NN.Rmd","Zc_model_runs_NN_density.Rmd")
-#, 
-            
-# myList = c("Gg_model_runs_NN_density.Rmd","Pm_model_runs_NN_density.Rmd",
-#            "Pm_model_runs_NN.Rmd","Gg_model_runs_NN.Rmd","Gmsp_model_runs_NN_density.Rmd")
-           
- # myList =  c("Gg_model_runs_gam_density.Rmd",
- #             "Zc_model_runs_gam_density.Rmd",
- #             "Ssp_model_runs_gam_density.Rmd", "Pm_model_runs_gam_density.Rmd",
- #             "Kspp_model_runs_gam_density.Rmd","Gmsp_model_runs_gam_density.Rmd","Me_model_runs_gam_density.Rmd")
- # 
-  # myList =  c("Zc_model_runs_NN.Rmd",
-  #              "Ssp_model_runs_NN.Rmd", "Pm_model_runs_gam.Rmd",
-  #              "Kspp_model_runs_NN.Rmd", "Me_model_runs_NN.Rmd")#"Gg_model_runs_NN_density.Rmd", "Gg_model_runs_NN.Rmd",
-  #               # don't run because of NAs?
+#myList =  c("Zc_model_runs_NN_density.Rmd","Pm_model_runs_gam_density.Rmd","Zc_model_runs_gam_density.Rmd","Gg_model_runs_NN_density.Rmd","Gg_model_runs_GAM_density.Rmd")
+myList =  c("Pm_model_runs_NN.Rmd","Gg_model_runs_NN.Rmd","Zc_model_runs_NN.Rmd")#"Pm_model_runs_GAM.Rmd","Gg_model_runs_GAM.Rmd","Zc_model_runs_GAM.Rmd")
+#
+# myList = c("Gg_model_runs_NN_density.Rmd","Pm_model_runs_NN_density.Rmd","Pm_model_runs_gam.Rmd", "Zc_model_runs_gam.Rmd", "Gg_model_runs_gam.Rmd")
+#            "Pm_model_runs_NN.Rmd","Gg_model_runs_NN.Rmd","Gmsp_model_runs_NN_density.Rmd")"Gg_model_runs_gam_density.Rmd",
+#          "Zc_model_runs_NN.Rmd",
+# myList =  c("Gg_model_runs_gam_density.Rmd",
+#             "Zc_model_runs_gam_density.Rmd",
+#             "Ssp_model_runs_gam_density.Rmd", "Pm_model_runs_gam_density.Rmd",
+#             "Kspp_model_runs_gam_density.Rmd","Gmsp_model_runs_gam_density.Rmd","Me_model_runs_gam_density.Rmd")
+# 
+# myList =  c("Zc_model_runs_NN.Rmd",
+#              "Ssp_model_runs_NN.Rmd", "Pm_model_runs_gam.Rmd",
+#              "Kspp_model_runs_NN.Rmd", "Me_model_runs_NN.Rmd")#"Gg_model_runs_NN_density.Rmd", "Gg_model_runs_NN.Rmd",
+#               # don't run because of NAs?
 # myList =  c("Gg_model_runs_gam.Rmd","Zc_model_runs_gam.Rmd",
 #             "Ssp_model_runs_gam.Rmd","Pm_model_runs_gam.Rmd",
 #             "Kspp_model_runs_gam.Rmd","Gmsp_model_runs_gam.Rmd")# no sightings:"Me_model_runs_gam.Rmd"
 
-  # "Gmsp_model_runs_NN_density.Rmd", 
-  #              "Zc_model_runs_NN_density.Rmd", "Me_model_runs_NN_density.Rmd",
-  #              "Ssp_model_runs_NN_density.Rmd", "Pm_model_runs_gam_density.Rmd",
-  #              "Kspp_model_runs_NN_density.Rmd",
- # myList =  c( 
- #              "Zc_model_runs_NN.Rmd", 
- #              "Ssp_model_runs_NN.Rmd", "Pm_model_runs_NN.Rmd", "Pm_model_runs_NN_density.Rmd", 
- #              "Kspp_model_runs_NN.Rmd")#"Me_model_runs_NN_density.Rmd","Me_model_runs_NN.Rmd","Gmsp_model_runs_NN.Rmd", "Gg_model_runs_NN.Rmd",
- #    "Kspp_model_runs_gam_density.Rmd",
- #   "Kspp_model_runs_NN.Rmd",  "Kspp_model_runs_NN_density.Rmd",  
- #   "Me_model_runs_NN.Rmd",    "Me_model_runs_NN_density.Rmd",
- #   "Pm_model_runs_gam.Rmd",   "Pm_model_runs_gam_density.Rmd",
- #   "Pm_model_runs_NN.Rmd",    "Pm_model_runs_NN_density.Rmd",     
- #   "Zc_model_runs_gam.Rmd",   "Zc_model_runs_gam_density.Rmd",        
- #   "Zc_model_runs_NN.Rmd",    "Zc_model_runs_NNdensity.Rmd",
- #  "Gg_model_runs_gam.Rmd",  
- #  "Gg_model_runs_gam_density.Rmd",        
- #  "Gg_model_runs_NN.Rmd",    
- #                             "Gg_model_runs_NN_density.Rmd",
- #  "Ssp_model_runs_gam.Rmd",  
- #  "Ssp_model_runs_gam_density.Rmd",        
- #  "Ssp_model_runs_NN.Rmd",  
- #  "Ssp_model_runs_NN_density.Rmd",
- #  "Gmsp_model_runs_gam.Rmd",
- # "Gmsp_model_runs_gam_density.Rmd",        
- #  "Gmsp_model_runs_NN.Rmd", 
- # "Gmsp_model_runs_NN_density.Rmd")
+# "Gmsp_model_runs_NN_density.Rmd", 
+#              "Zc_model_runs_NN_density.Rmd", "Me_model_runs_NN_density.Rmd",
+#              "Ssp_model_runs_NN_density.Rmd", "Pm_model_runs_gam_density.Rmd",
+#              "Kspp_model_runs_NN_density.Rmd",
+# myList =  c( 
+#              "Zc_model_runs_NN.Rmd", 
+#              "Ssp_model_runs_NN.Rmd", "Pm_model_runs_NN.Rmd", "Pm_model_runs_NN_density.Rmd", 
+#              "Kspp_model_runs_NN.Rmd")#"Me_model_runs_NN_density.Rmd","Me_model_runs_NN.Rmd","Gmsp_model_runs_NN.Rmd", "Gg_model_runs_NN.Rmd",
+#    "Kspp_model_runs_gam_density.Rmd",
+#   "Kspp_model_runs_NN.Rmd",  "Kspp_model_runs_NN_density.Rmd",  
+#   "Me_model_runs_NN.Rmd",    "Me_model_runs_NN_density.Rmd",
+#   "Pm_model_runs_gam.Rmd",   "Pm_model_runs_gam_density.Rmd",
+#   "Pm_model_runs_NN.Rmd",    "Pm_model_runs_NN_density.Rmd",     
+#   "Zc_model_runs_gam.Rmd",   "Zc_model_runs_gam_density.Rmd",        
+#   "Zc_model_runs_NN.Rmd",    "Zc_model_runs_NNdensity.Rmd",
+#  "Gg_model_runs_gam.Rmd",  
+#  "Gg_model_runs_gam_density.Rmd",        
+#  "Gg_model_runs_NN.Rmd",    
+#                             "Gg_model_runs_NN_density.Rmd",
+#  "Ssp_model_runs_gam.Rmd",  
+#  "Ssp_model_runs_gam_density.Rmd",        
+#  "Ssp_model_runs_NN.Rmd",  
+#  "Ssp_model_runs_NN_density.Rmd",
+#  "Gmsp_model_runs_gam.Rmd",
+# "Gmsp_model_runs_gam_density.Rmd",        
+#  "Gmsp_model_runs_NN.Rmd", 
+# "Gmsp_model_runs_NN_density.Rmd")
 
 for (f in myList) rmarkdown::render(f)
